f380919fb7d36650830dd7c8c103dbe93c60e084
Semidime <semidime@protonmail.com>
2023-02-15 16:03:13 +0000

    Adds getUserChoice function
    Function prompts user to enter choice of weapon.

--- rocks.js
+++ rocks.js
@@ -16,5 +16,37 @@ function getComputerChoice() {
         rPS = "Scissors"
     }
 
+    console.log(rPS); /* debugging */
+
     return rPS
+}
+
+/* function to prompt user to input selection,
+input will be case insensitive, will return prompt if the
+input is not "rock" "paper" or "scissors" */
+function getUserChoice() {
+    let rawInput = prompt("Please choose your weapon!");
+
+    console.log(rawInput); /* debugging */
+
+    /* capitalise input string */
+
+    let capitalizedInput = rawInput.substr(0,1).toUpperCase() + rawInput.substring(1).toLowerCase();
+
+    console.log(capitalizedInput); /* debugging */
+
+    /* check if user has entered Rocks, Paper or Scissors
+    if any other answer provided generate error and ask them to choose again */
+
+
+
+}
+
+/* playRound function.  requires two parameters
+playerSelection and computerSelection*/
+
+function playRound(computerSelection, playerSelection) {
+
+     
+
 }
